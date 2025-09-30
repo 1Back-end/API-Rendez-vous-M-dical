20bfaf14e579038e5bb1b269bc241407535e831e
Laurent Alphonse <laurentalphonsewilfried@gmail.com>
2025-09-30 20:03:33 +0100

    Gestion des consultatnts

--- app/main/crud/__init__.py
+++ app/main/crud/__init__.py
@@ -5,3 +5,9 @@ from .storage_crud import *
 from .user_crud import *
 from .adress_crud import *
 from .sexe_crud import *
+from .sexe_crud import *
+from .titres_crud import *
+from .religion_crud import *
+from .hopitals_crud import *
+from .specialite_crud import *
+from .service_hopitals_crud import *
