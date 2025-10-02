9f2bd6810253ac2845e2cc758fdff9e750be98b1
Dev-hub360 <oscaressonne@gmail.com>
2025-10-02 19:32:03 +0100

    Commeit fianl

--- app/main/schemas/__init__.py
+++ app/main/schemas/__init__.py
@@ -4,7 +4,7 @@ from .msg import Msg, BoolStatus, DataDisplay
 from .base import DataList
 from .user import *
 from .file import *
-from .adress import *
+from .address import *
 from .sexe import *
 from .titres import *
 from .religion import *
@@ -14,18 +14,3 @@ from .service_hopitals import *
 from .consultants import *
 from .patients import *
 from .rendez_vous import *
-
-
-
-
-
- 
-=======
-from .address import * 
-from .sexe import *
-from .hopitals import *
-from .religion import *
-from .specialite import *
-from .titres import *
->>>>>>> 79ec58cd0c1a2119c875d4d68ce9c032b285010a
-
